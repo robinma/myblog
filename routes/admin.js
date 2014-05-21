581cd2f6e9dbf96452f34bf88dbb36b58b98e617
robinma <ahmzj@163.com>
2014-05-21 21:30:23 +0800

    push files

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,8 @@ var router=express.Router()
 
 /*get admin index*/
 router.get('/',function(req,res){
-	res.render('admin/index',{})
+	//res.render('admin/index',{})
+  res.render('admin/login',{})
 });
 
 module.exports=router;
